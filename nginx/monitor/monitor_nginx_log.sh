6454cb49592905b7980343932a6985c52f52b28c
root <root@dev2.doit>
2021-11-30 20:29:19 +0800

    修复命令行参数

--- nginx/monitor/monitor_nginx_log.sh
+++ nginx/monitor/monitor_nginx_log.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
-# $1 为https://sct.ftqq.com/的SENDKEY
-# urlencode2() {
-#     # urlencode <string>
 
-#     old_lc_collate=$LC_COLLATE
-#     LC_COLLATE=C
-
-#     local length="${#1}"
-#     for (( i = 0; i < length; i++ )); do
-#         local c="${1:$i:1}"
-#         case $c in
-#             [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
-#             *) printf '%%%02X' "'$c" ;;
-#         esac
-#     done
-
-#     LC_COLLATE=$old_lc_collate
-# }
+# $1 为文件路径，/usr/local/openresty/nginx/logs/test.error.log
+# $2 为https://sct.ftqq.com/的SENDKEY
 
 #日志文件路径
-logfile=/usr/local/openresty/nginx/logs/
-filename=test.error.log
+logfile=$1
+sendkey=$2
+hostname=`hostname`
 
 #当天日期,年月日
 cur_date=`date +"%Y/%m/%d"`
@@ -32,12 +18,11 @@ start_time=`date -d"3 minutes ago" +"%H:%M:%S"`
 stop_time=`date +"%H:%M:%S"`
 
 #新增的错误日志
-error=`tac $logfile/$filename | awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{t=$2;t1=$1; if(dt==t1 && t>=st && t<=et) {print $0}}'`
+error=`tac $logfile | awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{t=$2;t1=$1; if(dt==t1 && t>=st && t<=et) {print $0}}'`
 if [ ! -n "$error" ]; then  
   exit 0 
 fi    
 
-error=`urlencode $error`
-
+error=`urlencode $hostname $error`
 # echo $error
-curl -G https://sctapi.ftqq.com/{$1}.send --data-urlencode title=$error --data-urlencode desp=$error
+curl -G https://sctapi.ftqq.com/{$sendkey}.send --data-urlencode title=$error --data-urlencode desp=$error
