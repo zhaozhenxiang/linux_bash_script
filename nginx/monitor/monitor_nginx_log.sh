a0c28ac1080a5b3bf80be7a149799c5b539deb72
xiangzi <854850613@qq.com>
2021-11-30 19:56:42 +0800

    nginx错误日志脚本

---
+++ nginx/monitor/monitor_nginx_log.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# $1 为https://sct.ftqq.com/的SENDKEY
+# urlencode2() {
+#     # urlencode <string>
+
+#     old_lc_collate=$LC_COLLATE
+#     LC_COLLATE=C
+
+#     local length="${#1}"
+#     for (( i = 0; i < length; i++ )); do
+#         local c="${1:$i:1}"
+#         case $c in
+#             [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+#             *) printf '%%%02X' "'$c" ;;
+#         esac
+#     done
+
+#     LC_COLLATE=$old_lc_collate
+# }
+
+#日志文件路径
+logfile=/usr/local/openresty/nginx/logs/
+filename=test.error.log
+
+#当天日期,年月日
+cur_date=`date +"%Y/%m/%d"`
+
+#开始时间（3分钟前）,时分秒
+start_time=`date -d"3 minutes ago" +"%H:%M:%S"`
+
+#结束时间,时分秒
+stop_time=`date +"%H:%M:%S"`
+
+#新增的错误日志
+error=`tac $logfile/$filename | awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{t=$2;t1=$1; if(dt==t1 && t>=st && t<=et) {print $0}}'`
+error=`urlencode $error`
+
+# echo $error
+curl -G https://sctapi.ftqq.com/{$1}.send --data-urlencode title=$error --data-urlencode desp=$error
---
+++ nginx/monitor/readme.md
@@ -0,0 +1,5 @@
+# 监控error log文件
+
+* yum install gridsite-clients    
+* crontab 每三分钟调用一次
+* ./monitor_nginx_log.sh SENDKEY
