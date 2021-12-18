72fa0e6388a26fda6de4d0c54f8fef6fbf865693
xiangzi <854850613@qq.com>
2021-12-18 18:22:32 +0800

    use custom wx push

--- php/laravel/laravel_error.sh
+++ php/laravel/laravel_error.sh
@@ -11,27 +11,27 @@ fi
 logfile=$1
 sendkey=$2
 hostname=`hostname`
-
+echo $logfile
 #当天日期,年月日
 cur_date=`date +"%Y/%m/%d"`
 
 #开始时间（1分钟前）,时分秒
-start_time=`date -d"1 minutes ago" +"%H:%M:%S"`
+start_time=`date -d"100 minutes ago" +"%H:%M:%S"`
 
 #结束时间,时分秒
 stop_time=`date +"%H:%M:%S"`
 
 #新增的错误日志
 #error=`tac $logfile | awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{t=$2;t1=$1; if(dt==t1 && t>=st && t<=et) {{for (i=6;i<=NF;i++)printf("%s ", $i);print ""}}}'|uniq`
-
 #error=`tac $logfile | awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{t=$2;t1=$1; if(dt==t1 && t>=st && t<=et) {print $0}}'|grep -Eo "request.+?HTTP"|uniq -uc`
-error=`tail -n100 $logfile |grep 500| awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{t=$2;t1=$1; if(dt==t1 && t>=st && t<=et) {for (i=8;i<=NF;i++)printf("%s ", $i);print ""}}'|grep -Eo "request.+?HTTP"|uniq -uc`
-
+#error=`tail -n10000 $logfile | awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{t=$2;t1=$1; if(dt==t1 && t>=st && t<=et) {print $0}}'|grep -Eo "error:"`
+#error=`tail -n10000 $logfile | awk -v st="$start_time" -v et="$stop_time" -v dt="$cur_date" '{print $2}'`
+error=`tail -n1000 $logfile |grep -E "local.ERROR:" -A2`
 if [ ! -n "$error" ]; then  
   exit 0 
 fi    
 #echo $error
+#exit
 error=`urlencode $error`
 # echo $error
-# curl -G https://sctapi.ftqq.com/{$sendkey}.send --data-urlencode title=$hostname --data-urlencode desp=$error
-curl -G http://tui.doit.am/dmp/html/tui_send_interface.php --data-urlencode sckey=$sendkey --data-urlencode title=$hostname --data-urlencode content=$error
+curl -G https://sctapi.ftqq.com/{$sendkey}.send --data-urlencode title=$hostname --data-urlencode desp=$error
